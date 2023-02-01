b47f159319247e9870fccbeea13263621f13d6e9
gen7 <giuseppe2.gentile@mail.polimi.it>
2023-02-01 19:26:29 +0100

    Build

---
+++ build/CMakeFiles/navierstokes.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/navierstokes.dir/src/NavierStokesSolver.cpp.o"
+  "CMakeFiles/navierstokes.dir/src/NavierStokesSolver.cpp.o.d"
+  "CMakeFiles/navierstokes.dir/src/main.cpp.o"
+  "CMakeFiles/navierstokes.dir/src/main.cpp.o.d"
+  "navierstokes"
+  "navierstokes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/navierstokes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/navierstokes.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for navierstokes.
+# This may be replaced when dependencies are built.
